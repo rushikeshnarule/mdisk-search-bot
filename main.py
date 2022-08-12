4e756da8972007871a8d0e7a0e142cdd3f86eba3
Maulik Patel <69646201+Maulikpatell@users.noreply.github.com>
2022-08-13 02:00:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -70,12 +70,10 @@ async def inline_handlers(_, event: Message):
                 msg_text = message.text.html.split("|||", 1)[0]
             answers += f'**🎬 Title ➠ ' + '' + f_text.split("\n", 1)[0] + '' + '\n\n📜 About ➠ ' + '' + f_text.split("\n", 2)[-1] + ' \n\n**'
     try:
-        await event.reply_text(
-            answers
-        )
+        await event.reply_text(answers)
         print(f"[{Config.BOT_SESSION_NAME}] - Answered Successfully - {event.from_user.first_name}")
         await asyncio.sleep(10)
-        await event.text.msg.delete(10)
+        await msg.delete(10)
     except:
         print(f"[{Config.BOT_SESSION_NAME}] - Failed to Answer - {event.from_user.first_name}")
 	
