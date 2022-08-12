36fe2943ac8d52c8480c33d2db3996103999cad5
Maulik Patel <69646201+Maulikpatell@users.noreply.github.com>
2022-08-13 03:35:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -75,8 +75,7 @@ async def inline_handlers(_, event: Message):
         await asyncio.sleep(10)
         await a.message.delete()
     except:
-        print(f"[{Config.BOT_SESSION_NAME}] - Failed to Answer - {event.from_user.first_name}")	
-	await asyncio.sleep(10)
+        print(f"[{Config.BOT_SESSION_NAME}] - Failed to Answer - {event.from_user.first_name}")		
         await a.message.delete()
 	     
 
